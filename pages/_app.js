a330994828f113386028454b8e5c9c5203664934
Mark Biesheuvel <mail@markbiesheuvel.nl>
2022-05-20 09:56:56 +0200

    Remove non essentials and add Bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
-import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const PassthroughComponent = ({ Component, pageProps }) => <Component {...pageProps} />;
 
-export default MyApp
+export default PassthroughComponent;
