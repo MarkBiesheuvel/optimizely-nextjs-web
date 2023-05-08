66cf456a02e2b18246a29ee9ba92c890acab0272
Mark Biesheuvel <mail@markbiesheuvel.nl>
2023-05-08 13:16:46 +0200

    Work around issue with behavioral audiences in SPA

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect } from 'react';
 import Layout from '../components/layout'
 
 const App = ({ Component, pageProps }) => {
+  useEffect(() => {
+    window.optimizely.push({type: 'activate'});
+  });
+
   return (
     <Layout>
       <Component {...pageProps} />
